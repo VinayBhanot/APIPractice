31682f470513a8605847d7e928eae68b4614d95a
hp <hp@LAPTOP-D75CE3BM>
2022-08-28 13:03:36 +0530

    API practice (28th Aug)

---
+++ Automate/src/test/java/org/testing/Testcases/TC_1GetMethod.java
@@ -0,0 +1,29 @@
+package org.testing.Testcases;
+
+import static io.restassured.RestAssured.*;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+//import io.restassured.http.ContentType;
+
+public class TC_1GetMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Response res = 
+        given()
+        .contentType(ContentType.JSON)
+        .when()
+        .get("http://localhost:3000/SeleniumStudets");
+        System.out.println("Status code is ");
+        System.out.println(res.statusCode());
+        System.out.println("Response data");
+        System.out.println(res.asString());
+        
+                 
+        		
+        		
+	}
+
+}
